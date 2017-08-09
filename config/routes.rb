a8e7b64af3d8269c9cc19d643eb98b2ab3eda234
Rubysdad <markandtabithamorales@gmail.com>
2017-08-09 23:34:41 +0000

    implemented stripe for payments

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,14 @@
 Rails.application.routes.draw do
   
+  resources :charges, only: [:new, :create]
+  get 'thanks', to: 'charges#thanks', as: 'thanks'
+  
   devise_for :users
+  
   resources :posts
+  
   root to: 'pages#home'
+  
   get 'creatives/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
