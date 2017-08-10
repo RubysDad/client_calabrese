59f126b408993a9101ecf76a73c2767fcfb1be25
Rubysdad <markandtabithamorales@gmail.com>
2017-08-10 12:46:35 +0000

    Monthly subscriptions on stripe

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -2,17 +2,24 @@ class ChargesController < ApplicationController
   before_action :amount_to_be_charged
   before_action :set_description
   before_action :authenticate_user!
+  before_action :set_plan
   
   def new
   end
 
   def create
-    customer = StripeTool.create_customer(email: params[:stripeEmail],
-                                          stripe_token: params[:stripeToken])
-    
-    charge = StripeTool.create_charge(customer_id: customer.id,
-                                      amount: @amount,
-                                      description: @description)
+    if params[:subscription].include? 'yes'
+      StripeTool.create_membership(email: params[:stripeEmail],
+                                   stripe_token: params[:stripeToken],
+                                   plan: @plan)
+    else
+      customer = StripeTool.create_customer(email: params[:stripeEmail],
+                                            stripe_token: params[:stripeToken])
+      
+      charge = StripeTool.create_charge(customer_id: customer.id,
+                                        amount: @amount,
+                                        description: @description)
+    end
     redirect_to thanks_path
   rescue Stripe::CardError => e
     flash[:error] = e.message
@@ -25,10 +32,14 @@ class ChargesController < ApplicationController
   private
   
     def amount_to_be_charged
-      @amount = 500
+      @amount = 2900
     end
     
     def set_description
-      @description = "Some amazing product"
+      @description = "Monthly Subscription"
+    end
+    
+    def set_plan
+      @plan = 9999
     end
 end
--- app/models/concerns/stripe_tool.rb
+++ app/models/concerns/stripe_tool.rb
@@ -15,4 +15,12 @@ module StripeTool
       currency: 'usd'
     )
   end
+  
+  def self.create_membership(email: email, stripe_token: stripe_token, plan: plan)
+    Stripe::Customer.create(
+      email:email,
+      source: stripe_token,
+      plan: plan
+    )
+  end
 end
